5e255cfc7e57a1318932416b6631056d4e7504f8
nam nguyen <n675n055@ku.edu>
2023-04-27 22:52:15 -0500

    displays current weather and forecast weather

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+var apiKey = "dd3a891209863bb45d12f6ff64e74adf";
+var searchHistory = JSON.parse(localStorage.getItem("search"));
+
+
+function getCoordinates(city) {
+	var coordinateUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + encodeURI(city) + "&limit=1&appid=" + apiKey;
+
+	fetch(coordinateUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			getWeather(data[0].lat, data[0].lon);
+		});
+}
+
+function getWeather(lat, lon) {
+	var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+
+	fetch(weatherUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			$("#cityName").text(data.name + " " + dayjs().format("M/D/YYYY"));
+			$("#icon").attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+			$("#temp").text("Temp: " + data.main.temp);
+			$("#wind").text("Wind: " + data.wind.speed);
+			$("#humidity").text("Humidity: " + data.main.humidity);
+			getForecast(lat, lon);
+		});
+}
+
+function getForecast(lat, lon) {
+	var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+
+	fetch(weatherUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			var counter = 1;
+			for (var i=4; i<=36; i=i+8) {
+				var forecastDay = data.list[i];
+				console.log(forecastDay);
+				$("#day"+counter).text(dayjs().add(counter, 'day').format("M/D/YYYY"));
+				$("#icon"+counter).attr("src", "http://openweathermap.org/img/wn/" + forecastDay.weather[0].icon + "@2x.png");
+				$("#temp"+counter).text("Temp: " + forecastDay.main.temp);
+				$("#wind"+counter).text("Wind: " + forecastDay.wind.speed);
+				$("#humidity"+counter).text("Humidity: " + forecastDay.main.humidity);
+				counter++;
+			}
+			$("#weatherDisplay").css("visibility", "visible");
+		});
+}
+
+function displaySearchHistory() {
+	
+}
+
+$("#citySubmit").on( "submit", function(event) {
+	event.preventDefault();
+  getCoordinates($("#city").val());
+
+} );
+
+displaySearchHistory();
